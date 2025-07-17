ef2bded9b6661804f9f35674d052eb8337f0d713
Muhammad Mahfuzur Rahman <mahfuzur993.rahman@gmail.com>
2025-07-17 20:16:41 +0100

    DVC added succesfully

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from dagshub import init  # ✅ Add this
+init(
+    repo_owner='mahfuzrahmandsuh23',
+    repo_name='E2E_DL_Mlflow_DVC_DagusHub_AWS',
+    mlflow=True
+)
+
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_modelling import PrepareModellingTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import ModelEvaluationPipeline
 
 
-
-
+# ========== Stage 1 ==========
 STAGE_NAME = "Data Ingestion stage"
-
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -20,12 +24,8 @@ except Exception as e:
     raise e    
 
 
-
-
-
+# ========== Stage 2 ==========
 STAGE_NAME = "Prepare base model"
-
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareModellingTrainingPipeline()
@@ -36,11 +36,23 @@ except Exception as e:
     raise e    
 
 
+# ========== Stage 3 ==========
 STAGE_NAME = "Model Training"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
+
+
+# ========== Stage 4 ==========
+STAGE_NAME = "Model Evaluation"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelEvaluationPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os
 from cnnClassifier.constants import *
-from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig)
+from cnnClassifier.utils.common import read_yaml, create_directories,save_json
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig,EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -72,4 +72,16 @@ class ConfigurationManager:
             params_learning_rate=params.LEARNING_RATE
         )
 
-        return training_config
+        return training_config 
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/mahfuzrahmandsuh23/E2E_DL_Mlflow_DVC_DagusHub_AWS.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
